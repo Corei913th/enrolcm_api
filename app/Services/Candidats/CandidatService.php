2d13b3df7e2af1e6d1f3de83ff25e9984724a316
Corei913th <bogningfredy972@gmail.com>
2025-12-29 15:41:56 +0100

    Revert "Merge branch 'feature/candidats' of https://github.com/Corei913th/enrolcm-api into feature/auth"
    This reverts commit 39895b9daf80efee1e402a25c67cae6afa254461, reversing
    changes made to ff5c3c8f2a23661b2611c3da51e729617642d7b2.

--- app/Services/Candidats/CandidatService.php
+++ app/Services/Candidats/CandidatService.php
@@ -7,42 +7,36 @@ use App\Models\Candidat;
 use App\Models\Utilisateur;
 use App\Services\Roles\RoleService;
 use App\Services\Users\UserService;
-use Exception;
 use Illuminate\Support\Facades\DB;
 
 class  CandidatService
 {
-
+    
     public function __construct(
         private readonly UserService $users,
         private readonly RoleService $roles,
     ) {}
+    
 
+     public function createPartial(CreateCandidatAccountDTO $dto): Utilisateur
+    {
+        return DB::transaction(function () use ($dto) {
+            $user = $this->users->createCandidatAccount($dto);
 
-    public function createPartialCandidat(
-        CreateCandidatAccountDTO $dto,
-        ?Utilisateur $user = null
-    ): Utilisateur {
-        return DB::transaction(function () use ($dto, $user) {
-            
-            $existingCandidat = Candidat::where('numero_recu', $dto->user_name)->first();
+            $existingCandidat = Candidat::where('numero_recu', $user->user_name)->first();
             if ($existingCandidat) {
-                throw new Exception(
-                    'Un candidat avec ce numéro de reçu existe déjà.'
-                );
+                throw new \Exception('Un candidat avec ce numéro de reçu existe déjà.');
             }
-            
-            $user ??= $this->users->createCandidatAccount($dto);
-           
-            $candidat = Candidat::create([
-                'utilisateur_id'     => $user->id,
-                'numero_recu'        => $dto->user_name,
-                'nationalite_cand'   => $dto->nationalite_cand,
-            ]);
-            
+
+             $candidats = Candidat::create([
+                'utilisateur_id' => $user->id,
+                'numero_recu' => $dto->user_name,
+                'nationalite_cand' => $dto->nationalite_cand,
+             ]);
+
             $this->roles->assignDefault($user, 'CANDIDAT');
 
-            return $user->setRelation('candidat', $candidat);
+            return $user->setRelation('candidat', $candidats);
         });
     }
 }
