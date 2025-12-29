0dcd29205b8f8dc407510be4e24f6361aa96e09a
Corei913th <bogningfredy972@gmail.com>
2025-12-29 17:45:23 +0100

    feat(users): implement refresh token logic,now  waiting for implementaion in feat(auth), config role middleware(ADMIN) in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum', 'admin'])
+Route::middleware(['auth:sanctum', 'role:ADMIN'])
     ->prefix('users')
     ->group(base_path('routes/api/users.php'));
 
