2c0849cb4437e920e0ce17a4963c9806eccabe0b
Corei913th <bogningfredy972@gmail.com>
2025-12-31 23:16:01 +0100

    feat: inclure routes départements dans api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-
-    
+  require __DIR__ . '/api/departements.php';
 });
