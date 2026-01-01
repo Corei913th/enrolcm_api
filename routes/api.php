29066db291d26d782adb28254b1da884034c3759
Corei913th <bogningfredy972@gmail.com>
2026-01-01 10:22:00 +0100

    chore: update routes/api.php, update departementService (create, update)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-  require __DIR__ . '/api/departements.php';
-});
+
+
+require __DIR__ . '/api/departements.php';
