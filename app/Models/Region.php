71a52129c908b96660ea8c1d52c634e414f27ad9
Corei913th <bogningfredy972@gmail.com>
2026-01-02 06:05:16 +0100

    feat: migration vers table regions avec relation region_id
    - Ajout du modèle Region avec enum RegionCameroun
    - Création de la table regions avec code unique
    - Ajout de region_id dans centres avec foreign key
    - Adaptation des scopes Centre pour utiliser region_id via relation
    - Mise à jour de getAdresseComplete() et estDansLaRegion()
    - Ajout de RegionRessource et exposition dans CentreResource
    - Retrait du cast region vers RegionCameroun (utilisation de la relation)

---
+++ app/Models/Region.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\RegionCameroun;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Region extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'regions';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    /**
+     * Les champs assignables en masse
+     */
+    protected $fillable = [
+        'code',
+        'libelle',
+        'est_actif',
+    ];
+
+    /**
+     * Les types de données pour Eloquent
+     */
+    protected $casts = [
+        'est_actif' => 'boolean',
+        'libelle' => RegionCameroun::class,
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+
+    ];
+
+    
+    public function centres(): HasMany
+    {
+        return $this->hasMany(Centre::class);
+    }
+}
